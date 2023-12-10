59bd338b201483d5e14a8cd9a5bd8d0c7b80a169
makiwarner <makenna.m.warner@gmail.com>
2023-12-10 02:10:43 +0100

    connects and sends msg to iot hub every 5 secs

---
+++ IOT_Dev.py
@@ -0,0 +1,33 @@
+import json  # Add this import statement
+import time  # Import the time module
+from azure.iot.device import IoTHubDeviceClient,Message
+
+# Azure IoT Hub connection parameters
+device_connection_string = "HostName=WaterValves.azure-devices.net;DeviceId=Valve1;SharedAccessKey=k/DtGT+J60IbObQGsfVU6oDSiKHmiWOPAAIoTBVYwOU="
+
+# Create an instance of the device client
+client = IoTHubDeviceClient.create_from_connection_string(device_connection_string)
+
+# Connect to Azure IoT Hub
+client.connect()
+
+try:
+    while True:  # Infinite loop
+        # Your JSON payload (replace this with your actual data)
+        json_payload = {"ValveStatus": 1}
+
+        # Send the JSON payload to Azure IoT Hub
+        client.send_message(json.dumps(json_payload))
+
+        print("Message sent:", json_payload)
+
+        # Wait for a specified time before sending the next message
+        time.sleep(5)  # Sleep for 5 seconds (adjust as needed)
+
+
+except KeyboardInterrupt:
+    # Handle keyboard interrupt (e.g., Ctrl+C) to gracefully exit the loop
+    pass
+finally:
+    # Disconnect from Azure IoT Hub
+    client.disconnect()
